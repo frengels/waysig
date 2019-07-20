75d5f15f1448eafa16876072fe853dde66d98c60
Frederik Engels <frederik.engels92@gmail.com>
2019-07-20 04:27:54 +0200

    implement equivalent to wl_listener

---
+++ include/waysig/slot.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <tuple>
+
+#include "detail/link.hpp"
+
+namespace ws
+{
+struct slot_base_impl : detail::link
+{
+    using function_type = void (*)(slot_base_impl*, void* data);
+
+    function_type func;
+
+    constexpr void operator()(void* data)
+    {
+        func(this, data);
+    }
+
+    template<typename... Args>
+    constexpr void operator()(Args&&... args)
+    {
+        auto fwd_args = std::forward_as_tuple(std::forward<Args>(args)...);
+        func(this, &fwd_args);
+    }
+};
+} // namespace ws

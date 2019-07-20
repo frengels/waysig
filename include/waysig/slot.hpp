91051a4d1c66e040f175032cc472bc526d1b5831
Frederik Engels <frederik.engels92@gmail.com>
2019-07-20 05:11:09 +0200

    provide constructors and specify link offset

--- include/waysig/slot.hpp
+++ include/waysig/slot.hpp
@@ -2,6 +2,7 @@
 
 #include <tuple>
 
+#include "detail/intrusive_list.hpp"
 #include "detail/link.hpp"
 
 namespace ws
@@ -12,10 +13,14 @@ public:
     using function_type = void (*)(slot_base_impl*, void* data);
 
 public:
-    ws::detail::link link;
-    function_type    func;
+    ws::detail::link link{};
+    function_type    func{nullptr};
 
 public:
+    constexpr slot_base_impl() noexcept = default;
+    constexpr slot_base_impl(function_type fn) noexcept : func{fn}
+    {}
+
     constexpr void operator()(void* data)
     {
         func(this, data);
@@ -28,4 +33,12 @@ public:
         func(this, &fwd_args);
     }
 };
+
+namespace detail
+{
+template<>
+struct link_offset<ws::slot_base_impl>
+    : std::integral_constant<std::size_t, offsetof(ws::slot_base_impl, link)>
+{};
+} // namespace detail
 } // namespace ws

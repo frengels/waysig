7416af5b11f0471f4348d202919795bc6fb2cf00
Frederik Engels <frederik.engels92@gmail.com>
2019-07-20 05:06:59 +0200

    implement wl_list_remove for link

--- include/waysig/detail/link.hpp
+++ include/waysig/detail/link.hpp
@@ -11,9 +11,24 @@ public:
     link* next{nullptr};
 
 public:
+    // the default for the wayland link's empty state is to have both
+    // uninitialized or null, in this case they're default to null.
     constexpr link() noexcept = default;
+
+    // this constructor should be used for making a wayland compatible list. In
+    // lists the list link is made to reference itself upon initialization,
+    // creating a circular doubly linked list.
     constexpr link(link* prev, link* next) noexcept : prev{prev}, next{next}
     {}
+
+    constexpr void remove() noexcept
+    {
+        // identical to wayland implementation
+        prev->next = next;
+        next->prev = prev;
+        next       = nullptr;
+        prev       = nullptr;
+    }
 };
 } // namespace detail
 } // namespace ws

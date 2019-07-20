2c8cb4ccd1a98fe0d82b6592465048499496eddb
Frederik Engels <frederik.engels92@gmail.com>
2019-07-20 17:03:03 +0200

    fix up slot/signal friendship

--- include/waysig/detail/slot_base.hpp
+++ include/waysig/detail/slot_base.hpp
@@ -1,18 +1,18 @@
 #pragma once
 
-#include "waysig/detail/signal_base.hpp"
-
 namespace ws
 {
 namespace detail
 {
+class signal_base;
+
 /// This is the class which is binary compatible with wl_listener, this is
 /// verified through the provided unit tests. Any functions defined should
 /// function exactly the same as the wl_listener_* ones.
 struct slot_base
 {
-    friend class ::ws::signal_base;
-    friend class ::ws::detail::link_offset<slot_base>;
+    friend class ws::detail::signal_base;
+    friend class ws::detail::link_offset<slot_base>;
 
 public:
     using function_type = void (*)(slot_base*, void* data);
@@ -33,8 +33,8 @@ public:
 };
 
 template<>
-struct link_offset<ws::slot_base>
-    : std::integral_constant<std::size_t, offsetof(ws::slot_base, link)>
+struct link_offset<ws::detail::slot_base>
+    : std::integral_constant<std::size_t, offsetof(ws::detail::slot_base, link)>
 {};
 } // namespace detail
 } // namespace ws

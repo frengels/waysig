8a3edcc2c69c3fdf916ce160a75e420ed8d46003
Frederik Engels <frederik.engels92@gmail.com>
2019-07-21 02:03:46 +0200

    add a test for type safe signals

--- tests/signal.cpp
+++ tests/signal.cpp
@@ -2,23 +2,46 @@
 
 #include "waysig/detail/signal_base.hpp"
 #include "waysig/detail/slot_base.hpp"
+#include "waysig/signal.hpp"
+#include "waysig/slot.hpp"
 
 TEST_CASE("signal")
 {
-    int i = 0;
+    SECTION("base")
+    {
+        int i = 0;
 
-    ws::detail::signal_base<void> sig;
+        ws::detail::signal_base<void> sig;
 
-    ws::detail::slot_base<void> s0{[](auto* self, void* data) {
-        (void) self;
-        auto& i = *static_cast<int*>(data);
-        ++i;
-    }};
+        ws::detail::slot_base<void> s0{[](auto* self, void* data) {
+            (void) self;
+            auto& i = *static_cast<int*>(data);
+            ++i;
+        }};
 
-    REQUIRE(i == 0);
-    sig.emit(&i);
-    REQUIRE(i == 0);
-    sig.connect(s0);
-    sig.emit(&i);
-    REQUIRE(i == 1);
+        REQUIRE(i == 0);
+        sig.emit(&i);
+        REQUIRE(i == 0);
+        sig.connect(s0);
+        sig.emit(&i);
+        REQUIRE(i == 1);
+    }
+
+    SECTION("derived")
+    {
+        int                    i = 0;
+        ws::signal<void(int&)> sig;
+
+        ws::slot<void(int&)> s0{[](auto* self, int& i) {
+            (void) self;
+            ++i;
+        }};
+
+        REQUIRE(i == 0);
+        sig.emit(i);
+        REQUIRE(i == 0);
+        sig.connect(s0);
+        sig.emit(i);
+        REQUIRE(i == 1);
+    }
 }

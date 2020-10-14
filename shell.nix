53177ea817e250d48dd94fdd8876040681dd1e25
Frederik Engels <frederik.engels92@gmail.com>
2020-10-14 19:00:18 +0200

    remove nix files pre flakes

--- shell.nix
+++
@@ -1,6 +0,0 @@
-{ pkgs? import <nixpkgs> {}}:
-with pkgs;
-gcc9Stdenv.mkDerivation {
-    name = "waysig-env";
-    buildInputs = [ cmake pkgconfig catch2 wayland extra-cmake-modules clang gdb ];
-}
